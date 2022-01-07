24286f629cee3a43de715f72261b26019f824e63
MoisesLagos123 <moises.lagos.f@gmail.com>
2022-01-07 17:03:55 -0300

    se agrega varible de entorno

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+//seteo de variables de entorno
+dotenv.config({path: '.env'})
+
 //llamada al router
 app.get('/', require('./router'))
+
 //carpeta publica
 app.use(express.static('public'))
+
 // motor de plantillas
 app.set('view engine','ejs')
-app.listen(3000, ()=>{
-    console.log('SERVIDOR INICIADO EN: http://localhost:3000')
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log('SERVIDOR INICIADO EN: http://localhost:'+process.env.PORT)
 })
